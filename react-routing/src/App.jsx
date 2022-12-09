64fb874fa08cc9a3b5a0a8bf441912609fb01480
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-12-09 17:11:22 +0530

    caluclater

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,22 +1,9 @@
 import React from 'react'
-import {useState} from 'react'
-import Shop from './Component/Shop'
-import Cart from './Component/Cart'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import Navbar from './Component/Navbar'
-function App() {
-  const[show,setshow]=useState(true);
-const [cart,setcart]=useState(true);
+import '../src/style.css'
+const App = () => {
   return (
     <div>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Shop/>}/>
-          <Route path='/' element={<Cart/>}/>
-        </Routes>
-      </Router>
-
+      ok
     </div>
   )
 }
