dc3ca7b6137f371810de4ae38ce6343872fef994
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2023-05-24 13:29:23 +0530

    ok

---
+++ JAVASCRIPT/1.js
@@ -0,0 +1,7 @@
+// []  its object in js
+//{} array in javascript
+
+const object1 ={
+property1:20
+};
+console.log(Object.is(object1));
---
+++ JAVASCRIPT/mao.js
@@ -0,0 +1,4 @@
+let a=[100,200]
+let b=a.map(x=>x*2)
+console.table(b)
+console.table(a)
